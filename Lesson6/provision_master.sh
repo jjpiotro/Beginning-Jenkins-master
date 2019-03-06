49bf9cc3b49617756bd335bb09992f8b048a9e07
jjpiotro <jjpiotrowski92@gmail.com>
2019-03-06 09:19:34 +0100

    Jenkins Tutorial

---
+++ Lesson6/provision_master.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install Java
+sudo yum install java-1.8.0-openjdk -y
+java -version
+
+# Install Jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins -y
+
+# Start Jenkins & ensure Jenkins is started at startup
+sudo service jenkins start
+sudo chkconfig jenkins on
+
+# Disable Firewall To Allow Port Forwarding To Host
+# THIS IS NOT RECOMMENDED ON ANY OTHER ENVIRONMENT. INSTEAD CREATE RULES FOR TRAFFIC
+sudo service iptables stop
